146f61a1dc118a15cdb8e3934887c9f160ee27b2
Taqi <s.taqiuddin6987@gmail.com>
2023-06-20 16:42:05 +0500

    feat LA-40 login functionality add file is missing.

---
+++ src/services/superadmin/SuperAdminAuth.ts
@@ -0,0 +1,11 @@
+import network from '../../utils/network';
+import { SuperadminUserLogin } from '../../interfaces/superadmin/auth.interface';
+import { BACKOFFICE_PREFIX } from '../../utils/constants';
+
+const loginService = (userData: SuperadminUserLogin) => {
+    return network.post(BACKOFFICE_PREFIX + '/login', userData);
+}
+
+export default {
+    loginService
+}
