1fbe8dab96b5ca1828885a8da86130c7b02af70c
Rafayasad01 <rafay.asad@urapptech.com>
2023-09-07 10:32:02 +0500

    updated

--- src/pages/orders/OrderDetailsPage.tsx
+++ src/pages/orders/OrderDetailsPage.tsx
@@ -128,17 +128,31 @@ function OrderDetailsPage() {
   };
 
   const createOrderStatusesService = (data: any, key: string) => {
+    setIsLoader(true);
     if (key === ORDER_STATUS_IN_CANCELLED && (cancelled || isCancelled)) {
       return;
     }
     orderService.createStatusesService(data).then((item) => {
       if (item) {
+        setIsLoader(false);
+        setIsNotify(true);
+        setNotifyMessage({
+          text: item.data.message,
+          type: 'success',
+        });
         const tempData = viewData;
         tempData.appOrderStatuses.push(item.data.data);
         setViewData(tempData);
         setData(tempData);
       }
-    });
+    }).catch((err) => {
+      setIsLoader(false);
+      setIsNotify(true);
+      setNotifyMessage({
+        text: err.message,
+        type: 'error',
+      });
+    })
   };
 
   const statusUpdateHandler = () => {
@@ -196,12 +210,11 @@ function OrderDetailsPage() {
             <div className="p-4">
               <div className="flex items-center">
                 <div
-                  className={`relative mr-2 inline-flex ${
-                    currentStatus &&
+                  className={`relative mr-2 inline-flex ${currentStatus &&
                     currentStatus.key === ORDER_STATUS_IN_CANCELLED
-                      ? 'text-red-500'
-                      : 'text-green-500'
-                  }`}
+                    ? 'text-red-500'
+                    : 'text-green-500'
+                    }`}
                 >
                   <CircularProgress
                     thickness={1.5}
@@ -238,12 +251,11 @@ function OrderDetailsPage() {
                     )}
                   </div>
                   <div
-                    className={`font-open-sans text-sm font-semibold  ${
-                      currentStatus &&
+                    className={`font-open-sans text-sm font-semibold  ${currentStatus &&
                       currentStatus.key === ORDER_STATUS_IN_CANCELLED
-                        ? 'text-red-500'
-                        : 'text-green-500'
-                    } `}
+                      ? 'text-red-500'
+                      : 'text-green-500'
+                      } `}
                   >
                     {currentStatus && `${currentStatus.value.title} `}
                   </div>
@@ -255,11 +267,10 @@ function OrderDetailsPage() {
                     setDialogText('Are you sure you want to cancel this Order');
                     setCancelDialogOpen(true);
                   }}
-                  className={`rounded-xl py-2 px-12 font-open-sans text-sm font-semibold ${
-                    cancelled || isCancelled
-                      ? 'bg-neutral-400 text-neutral-900'
-                      : 'bg-neutral-900 text-gray-50'
-                  } `}
+                  className={`rounded-xl py-2 px-12 font-open-sans text-sm font-semibold ${cancelled || isCancelled
+                    ? 'bg-neutral-400 text-neutral-900'
+                    : 'bg-neutral-900 text-gray-50'
+                    } `}
                   color="inherit"
                   disabled={!!(cancelled || isCancelled)}
                 >
@@ -472,13 +483,12 @@ function OrderDetailsPage() {
                         );
                         setDialogOpen(true);
                       }}
-                      className={`rounded py-2 px-12 font-open-sans text-sm font-semibold ${
-                        cancelled ||
+                      className={`rounded py-2 px-12 font-open-sans text-sm font-semibold ${cancelled ||
                         (isCancelled &&
                           nextBtn.key === ORDER_STATUS_IN_CANCELLED)
-                          ? 'bg-neutral-400 text-neutral-900'
-                          : 'bg-neutral-900 text-gray-50'
-                      } `}
+                        ? 'bg-neutral-400 text-neutral-900'
+                        : 'bg-neutral-900 text-gray-50'
+                        } `}
                       color="inherit"
                       disabled={
                         !!(
@@ -503,9 +513,8 @@ function OrderDetailsPage() {
                   return (
                     <div
                       key={index}
-                      className={`flex items-center ${
-                        item.isStatus ? '' : 'opacity-25'
-                      } `}
+                      className={`flex items-center ${item.isStatus ? '' : 'opacity-25'
+                        } `}
                     >
                       {item.isStatus ? (
                         <CheckCircleOutlineOutlinedIcon />
@@ -514,9 +523,8 @@ function OrderDetailsPage() {
                       )}
 
                       <div
-                        className={`relative mx-2 inline flex ${
-                          item.isStatus ? item.value.color : 'text-neutral-500'
-                        } `}
+                        className={`relative mx-2 inline flex ${item.isStatus ? item.value.color : 'text-neutral-500'
+                          } `}
                       >
                         <CircularProgress
                           thickness={1.5}
@@ -532,11 +540,10 @@ function OrderDetailsPage() {
                       </div>
                       <div>
                         <div
-                          className={`font-open-sans text-base font-semibold ${
-                            item.isStatus
-                              ? item.value.color
-                              : 'text-neutral-500'
-                          } `}
+                          className={`font-open-sans text-base font-semibold ${item.isStatus
+                            ? item.value.color
+                            : 'text-neutral-500'
+                            } `}
                         >
                           {item.value.title}
                         </div>
