749ac3a554e323d75d4c5783a1289458f1ac0b37
Syed Taqiuddin <34369865+taqiuddin6987@users.noreply.github.com>
2023-09-28 14:12:45 +0500

    cosmatic changes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-//export const BASE_URL = import.meta.env.VITE_SERVICE_BASE_URL;
+// export const BASE_URL = import.meta.env.VITE_SERVICE_BASE_URL;
 export const BASE_URL = 'https://dev.urapptech.com/api/v1/admin/';
 export const GOOGLE_MAP_KEY = import.meta.env.VITE_GOOGLE_MAP_KEY;
 export const BACKOFFICE_PREFIX = 'backofficeUser';
