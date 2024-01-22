aa24f65cab19f5a141477dde47a70de7a7e86877
Shoaib Ahmed <shoaib@urapptech.com>
2024-01-22 16:49:10 +0500

    # Theme Config changes Manage Notification throughout components. Improvise Theme Form layout. Transform key into Pascal case.

--- src/pages/super-admin/theme-configurations/SuperAdminThemeForm.tsx
+++ src/pages/super-admin/theme-configurations/SuperAdminThemeForm.tsx
@@ -1,9 +1,9 @@
-import { memo } from 'react';
+import { memo, ChangeEvent } from 'react';
 import {
   Theme,
   ValidThemeColorKey,
 } from '../../../interfaces/superadmin/theme.interface';
-import { useForm, SubmitHandler  } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { Button, FormControl, Input } from '@mui/material';
 import ErrorSpanBox from '../../../components/common/ErrorSpanBox';
 import {
@@ -14,7 +14,7 @@ import {
   THEME_COLORS,
 } from '../../../utils/constants';
 import _ from 'lodash';
-import { useNavigate } from 'react-router-dom';;
+import { useNavigate } from 'react-router-dom';
 
 interface ThemeFormProps {
   onSubmit: SubmitHandler<Partial<Theme>>; // Submit handler that takes a Theme object
@@ -22,7 +22,11 @@ interface ThemeFormProps {
   theme: Theme | null;
 }
 
-const SuperAdminThemeForm: React.FC<ThemeFormProps> = ({ onSubmit, theme, editMode }) => {
+const SuperAdminThemeForm: React.FC<ThemeFormProps> = ({
+  onSubmit,
+  theme,
+  editMode,
+}) => {
   const navigate = useNavigate();
 
   const {
@@ -34,9 +38,35 @@ const SuperAdminThemeForm: React.FC<ThemeFormProps> = ({ onSubmit, theme, editMo
     control,
   } = useForm<Theme>();
 
-
   const StoreSubmit = (data: Partial<Theme>) => {
-    onSubmit(data);
+    // Check if theme.key exists in the data object
+    if (data && data.key) {
+      // Check if the value is already in the required format
+      const isFormatted = /^[A-Z][a-z]*(?:[A-Z][a-z]*)*$/.test(data.key);
+
+      // If not in the required format, modify the value
+      if (!isFormatted) {
+        // Remove spaces, capitalize first letter of every word, and join the words
+        const formattedKey = data.key
+          .replace(/\s+/g, ' ')
+          .trim()
+          .toLowerCase() // Convert the entire string to lowercase
+          .replace(/(?:^|\s)\S/g, (char) => char.toUpperCase())
+          .replace(/\s/g, ''); // Remove spaces
+
+        // Create a new object with the formatted key and the rest of the data
+        const formattedData = { ...data, key: formattedKey };
+
+        // Call the onSubmit function with the modified data
+        onSubmit(formattedData);
+      } else {
+        // Call the onSubmit function with the original data if theme.key is already in the required format
+        onSubmit(data);
+      }
+    } else {
+      // Call the onSubmit function with the original data if theme.key is not present
+      onSubmit(data);
+    }
   };
 
   return (
@@ -44,14 +74,14 @@ const SuperAdminThemeForm: React.FC<ThemeFormProps> = ({ onSubmit, theme, editMo
       <div className="FormBody">
         <div className="FormFields">
           <FormControl className="FormControl" variant="standard">
-            <label className="FormLabel">
+            <label className="font-bold">
               Key{' '}
               <span className="text-xs text-gray-400">
                 ( max 50 characters )
               </span>
             </label>
             <Input
-              className="FormInput"
+              className="FormInput m-0 h-[40px] w-[350px] rounded-lg border-2 border-[#949EAE] px-3 outline-none "
               {...register('key', {
                 required: true,
                 pattern: PATTERN.CHAR_SPACE_DASH,
@@ -59,7 +89,7 @@ const SuperAdminThemeForm: React.FC<ThemeFormProps> = ({ onSubmit, theme, editMo
                 value: theme?.key,
               })}
               type="text"
-              id="firstName"
+              id="key"
               placeholder="Enter Key"
               disableUnderline
             />
@@ -74,90 +104,93 @@ const SuperAdminThemeForm: React.FC<ThemeFormProps> = ({ onSubmit, theme, editMo
             )}
           </FormControl>
         </div>
-        <div className="mt-6">
-          <h2>Enter Theme Colors</h2>
 
-          <div className="mt-5 grid grid-cols-1 gap-2 sm:grid-cols-2  lg:grid-cols-6">
-            {THEME_COLORS.map((color: ValidThemeColorKey) => (
-              <FormControl
-                key={color}
-                className="FormControl"
-                variant="standard"
-              >
-                <label className="FormLabel">{`${color
-                  .charAt(0)
-                  .toUpperCase()}${color.slice(1)} Color:`}</label>
-                <Input
-                  className="FormInput"
-                  {...register(`value.themeColor.${color}`, {
-                    required: true,
-                    // pattern: PATTERN.CHAR_SPACE_DASH,
-                    value: theme?.value.themeColor?.[color],
-                    validate: (value) => value.length <= 50,
-                  })}
-                  disableUnderline
-                />
-                {errors.value?.themeColor?.[color] && (
-                  <ErrorSpanBox
-                    error={errors.value.themeColor[color]?.message}
+        <div className="mt-6 grid grid-cols-2  gap-5">
+          <div className="rounded-lg border-2 p-5">
+            <label className="font-bold">Enter Theme Colors</label>
+
+            <div className="grid grid-cols-1 gap-5 py-5 sm:grid-cols-2 md:grid-cols-2">
+              {THEME_COLORS.map((color: ValidThemeColorKey) => (
+                <FormControl
+                  key={color}
+                  className="FormControl"
+                  variant="standard"
+                >
+                  <label className="FormLabel">{`${color
+                    .charAt(0)
+                    .toUpperCase()}${color.slice(1)} Color:`}</label>
+                  <Input
+                    className="FormInput"
+                    {...register(`value.themeColor.${color}`, {
+                      required: true,
+                      // pattern: PATTERN.CHAR_SPACE_DASH,
+                      value: theme?.value.themeColor?.[color],
+                      validate: (value) => value.length <= 50,
+                    })}
+                    disableUnderline
                   />
-                )}
-                {errors.value?.themeColor?.[color]?.type === 'pattern' && (
-                  <ErrorSpanBox error={INVALID_CHAR} />
-                )}
-                {errors.value?.themeColor?.[color]?.type === 'validate' && (
-                  <ErrorSpanBox error={MAX_LENGTH_EXCEEDED} />
-                )}
-                {errors.value?.themeColor?.[color]?.type === 'required' && (
-                  <ErrorSpanBox
-                    error={`${_.upperFirst(color)} Color is required`}
-                  />
-                )}
-              </FormControl>
-            ))}
+                  {errors.value?.themeColor?.[color] && (
+                    <ErrorSpanBox
+                      error={errors.value.themeColor[color]?.message}
+                    />
+                  )}
+                  {errors.value?.themeColor?.[color]?.type === 'pattern' && (
+                    <ErrorSpanBox error={INVALID_CHAR} />
+                  )}
+                  {errors.value?.themeColor?.[color]?.type === 'validate' && (
+                    <ErrorSpanBox error={MAX_LENGTH_EXCEEDED} />
+                  )}
+                  {errors.value?.themeColor?.[color]?.type === 'required' && (
+                    <ErrorSpanBox
+                      error={`${_.upperFirst(color)} Color is required`}
+                    />
+                  )}
+                </FormControl>
+              ))}
+            </div>
           </div>
-        </div>
 
-        <div className="mt-6">
-          <h2>Enter Category color</h2>
+          <div className="rounded-lg border-2 p-5">
+            <label className="font-bold">Enter Category Colors</label>
 
-          <div className="mt-5 grid grid-cols-1 gap-2 sm:grid-cols-2  lg:grid-cols-6">
-            {[...Array(CATEGORY_COLORS_COUNT)].map((_, index) => (
-              <FormControl
-                key={index}
-                className="FormControl"
-                variant="standard"
-              >
-                <label className="FormLabel">{`Category Color ${
-                  index + 1
-                }:`}</label>
-                <Input
-                  className="FormInput"
-                  {...register(`value.categoryColor.${index}`, {
-                    required: true,
-                    value: theme?.value.categoryColor?.[index],
-                    //   pattern: PATTERN.CHAR_SPACE_DASH,
-                    validate: (value) => value?.length <= 50,
-                  })}
-                  disableUnderline
-                />
-                {errors.value?.categoryColor?.[index]?.type === 'required' && (
-                  <ErrorSpanBox
-                    error={`Category Color ${index + 1} is required`}
+            <div className="grid grid-cols-1 gap-5 py-5 sm:grid-cols-2 md:grid-cols-2">
+              {[...Array(CATEGORY_COLORS_COUNT)].map((_, index) => (
+                <FormControl
+                  key={index}
+                  className="FormControl"
+                  variant="standard"
+                >
+                  <label className="FormLabel">{`Category Color ${
+                    index + 1
+                  }:`}</label>
+                  <Input
+                    className="FormInput"
+                    {...register(`value.categoryColor.${index}`, {
+                      required: true,
+                      value: theme?.value.categoryColor?.[index],
+                      //   pattern: PATTERN.CHAR_SPACE_DASH,
+                      validate: (value) => value?.length <= 50,
+                    })}
+                    disableUnderline
                   />
-                )}
-                {errors.value?.categoryColor?.[index]?.type === 'pattern' && (
-                  <ErrorSpanBox error={INVALID_CHAR} />
-                )}
-                {errors.value?.categoryColor?.[index]?.type === 'validate' && (
-                  <ErrorSpanBox error={MAX_LENGTH_EXCEEDED} />
-                )}
-              </FormControl>
-            ))}
+                  {errors.value?.categoryColor?.[index]?.type ===
+                    'required' && (
+                    <ErrorSpanBox
+                      error={`Category Color ${index + 1} is required`}
+                    />
+                  )}
+                  {errors.value?.categoryColor?.[index]?.type === 'pattern' && (
+                    <ErrorSpanBox error={INVALID_CHAR} />
+                  )}
+                  {errors.value?.categoryColor?.[index]?.type ===
+                    'validate' && <ErrorSpanBox error={MAX_LENGTH_EXCEEDED} />}
+                </FormControl>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-      <div className="FormFooter">
+      <div className="mt-4 flex ">
         <Button
           className="btn-black-outline"
           type="button"
@@ -171,8 +204,8 @@ const SuperAdminThemeForm: React.FC<ThemeFormProps> = ({ onSubmit, theme, editMo
         </Button>
         <Input
           type="submit"
-          value={editMode ? "Update" : "Add" }
-          className="btn-black-fill"
+          value={editMode ? 'Update' : 'Add'}
+          className="btn-black-fill w-auto"
           disableUnderline
           sx={{
             padding: '0.375rem 2rem !important',
