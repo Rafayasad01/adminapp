0f0dad0dcc39f189b2e08b7fa5d2b543cb806f04
Syed Taqiuddin <34369865+taqiuddin6987@users.noreply.github.com>
2024-01-29 16:06:11 +0500

    All bugs are fixed.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/admin',
   plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
 });
