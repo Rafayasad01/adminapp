675b01faafe689d22cf7544c2ee4ba55b4290c74
Rafayasad <rafay.asad@urapptech.com>
2024-02-29 14:40:31 +0500

    updated host

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 import { setItem } from './storage';
 
 // const HOST = 'https://dev.urapptech.com';
-const HOST = 'http://192.168.8.68:3200';
-// const HOST = 'http://192.168.8.97:3200';
+// const HOST = 'http://192.168.8.68:3200';
+const HOST = 'http://192.168.8.97:3200';
 
 export const BASE_URL = `${HOST}/api/v1/admin/`;
 export const BASE_SYSTEM_URL = `${HOST}/api/v1/system/config/`;
