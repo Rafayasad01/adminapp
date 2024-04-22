83764d35b717d9a178dbb4627c4c099e032a2652
Shoaib Ahmed <shoaib@urapptech.com>
2024-04-22 16:50:49 +0500

    Constant file left

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -71,6 +71,7 @@ export const NOT_AUTHORIZED_MESSAGE = 'You dont have permission for this.';
 export const TOKEN_STORE_KEY = 'APP_AUTH_TOKEN';
 export const SYSTEM_CONFIG_PREFIX = 'theme';
 export const SHOP_SCHEDULING_PREFIX = 'shop-schedule';
+export const CURRENCY_PREFIX = '$';
 
 let TEXT_STORE_KEY = '';
 export const setText = (text: string) => {
