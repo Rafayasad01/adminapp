9c297cf35f62198e49cc731b7bcda3c6d9dc9328
Rafayasad <rafay.asad@urapptech.com>
2024-05-27 14:16:50 +0500

    minor changes:: all fields are updated to take all kind of text and only integer will take numbers only

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -337,23 +337,40 @@ export const weekDays = [
 
 // patterns
 export const PATTERN = {
-  // CHAR_NUM_DOT_AT: /^[A-Za-z0-9\s.@]+$/,
-  CHAR_NUM_DOT_AT: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, // used for email fields
-  CHAR_SPACE_DASH: /^[A-Za-z\s-]+$/, // used for textfield fields
-  CHAR_NUM_SPACE_DASH: /^[A-Za-z0-9\s-]+$/, // used for textfield fields
-  ADDRESS_ONLY: /^[A-Za-z0-9\s@.,#()-]+$/, // used for textfield address
-  CHAR_SPEC_NUM_DASH: /^[A-Za-z0-9\s@$.%!,#()-]+$/, // used for textfield address
-  CHAR_NUM_DASH: /^[A-Za-z0-9-]+$/, // used for only num,chars,dash like; postal code
-  NUM_PLUS_MINUS: /^[+-\d\s]+$/,
-  ACTION_WITHOUT_SPACE: /^[a-zA-Z0-9/-]+$/,
-  PASSWORD: /^[^\s]+$/,
+  CHAR_NUM_DOT_AT: /^[\s\S]+$/, // used for email fields
+  CHAR_SPACE_DASH: /^[\s\S]+$/, // used for textfield fields
+  CHAR_NUM_SPACE_DASH: /^[\s\S]+$/, // used for textfield fields
+  ADDRESS_ONLY: /^[\s\S]+$/, // used for textfield address
+  CHAR_SPEC_NUM_DASH: /^[\s\S]+$/, // used for textfield address
+  CHAR_NUM_DASH: /^[\s\S]+$/, // used for only num,chars,dash like; postal code
+  NUM_PLUS_MINUS: /^[\s\S]+$/,
+  ACTION_WITHOUT_SPACE: /^[\s\S]+$/,
+  PASSWORD: /^[\s\S]+$/,
   NUM_DASH: /^[0-9-]+$/, // used for num,dash type text
   PHONE: /^[\d()+-]*\d[\d()+-]*$/, // used for phone type text
   ONLY_NUM: /^\d+$/, // used for string type text
   ALLOW_ALL: /^[\s\S]+$/, // userd for allowed all
   POINT_NUM: /^[+-]?([0-9]*[.])?[0-9]+$/,
-  CHAR_NUM_MINUS_AT_SPACE: /^[a-zA-Z0-9@ -]+$/,
-  CHAR_NUM_MIN_AT_HASH_COM_DOT_SPA: /^[a-zA-Z0-9@,\-.# ]+$/,
+  CHAR_NUM_MINUS_AT_SPACE: /^[\s\S]+$/,
+  CHAR_NUM_MIN_AT_HASH_COM_DOT_SPA: /^[\s\S]+$/,
+
+  // CHAR_NUM_DOT_AT: /^[A-Za-z0-9\s.@]+$/,
+  // CHAR_NUM_DOT_AT: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, // used for email fields
+  // CHAR_SPACE_DASH: /^[A-Za-z\s-]+$/, // used for textfield fields
+  // CHAR_NUM_SPACE_DASH: /^[A-Za-z0-9\s-]+$/, // used for textfield fields
+  // ADDRESS_ONLY: /^[A-Za-z0-9\s@.,#()-]+$/, // used for textfield address
+  // CHAR_SPEC_NUM_DASH: /^[A-Za-z0-9\s@$.%!,#()-]+$/, // used for textfield address
+  // CHAR_NUM_DASH: /^[A-Za-z0-9-]+$/, // used for only num,chars,dash like; postal code
+  // NUM_PLUS_MINUS: /^[+-\d\s]+$/,
+  // ACTION_WITHOUT_SPACE: /^[a-zA-Z0-9/-]+$/,
+  // PASSWORD: /^[^\s]+$/,
+  // NUM_DASH: /^[0-9-]+$/, // used for num,dash type text
+  // PHONE: /^[\d()+-]*\d[\d()+-]*$/, // used for phone type text
+  // ONLY_NUM: /^\d+$/, // used for string type text
+  // ALLOW_ALL: /^[\s\S]+$/, // userd for allowed all
+  // POINT_NUM: /^[+-]?([0-9]*[.])?[0-9]+$/,
+  // CHAR_NUM_MINUS_AT_SPACE: /^[a-zA-Z0-9@ -]+$/,
+  // CHAR_NUM_MIN_AT_HASH_COM_DOT_SPA: /^[a-zA-Z0-9@,\-.# ]+$/,
 };
 
 export const MAX_LENGTH_EXCEEDED = 'Maximum length exceeded';
