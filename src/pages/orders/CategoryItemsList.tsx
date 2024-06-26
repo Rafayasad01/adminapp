13fbe54b84f7f42748929c195064c5115fe467be
Rafayasad <rafay.asad@urapptech.com>
2024-06-26 10:35:34 +0500

    UPDATE:: fixes of swiper

--- src/pages/orders/CategoryItemsList.tsx
+++ src/pages/orders/CategoryItemsList.tsx
@@ -69,10 +69,10 @@ const CategoryItemsList: React.FC<CategoryItemsListProps> = ({
             />
           </button>
           <div className="">
-            <span className="name text-base">{item.name}</span>
-            <span className="price text-[13px]">
+            <p className="name m-0 text-base">{item.name}</p>
+            <p className="price mb-3 text-[12px]">
               {CURRENCY_PREFIX} {item.price}
-            </span>
+            </p>
             <button
               className="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium btn-add css-sghohy-MuiButtonBase-root-MuiButton-root bg-primary"
               tabIndex={0}
