b01a58648c61902dcfd34cf55efe9c80503da819
Rafayasad <rafay.asad@urapptech.com>
2024-07-01 16:00:24 +0500

    update: login placeholder changed

--- src/pages/auth/login/LoginPage.tsx
+++ src/pages/auth/login/LoginPage.tsx
@@ -169,7 +169,7 @@ function LoginPage() {
                     <Input
                       className="border-1 border-solid border-secondary text-[11px]"
                       id="email"
-                      placeholder="example@mail.com"
+                      placeholder="user@example.com"
                       type="email"
                       {...register('email', {
                         required: 'Please enter your email.',
