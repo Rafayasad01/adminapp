3d7e7511bc714205e5971afca74c8f5610f4643b
Rafayasad <rafay.asad@urapptech.com>
2025-01-24 12:08:26 +0500

    update:: route issue on idle time

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
     const idleTime = dayjs.duration(15, 'minutes').asMilliseconds();
     monitorIdleTime(intervalTime, idleTime, () => {
       localStorage.clear();
-      window.location.replace('/');
+      window.location.replace('/admin');
     });
   }, []);
 
